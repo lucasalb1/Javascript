c89ed3d79285cfc565ff6a3bb653b940cccabdd6
lucasalb1 <lucasalb160@gmail.com>
2023-01-15 02:16:51 -0300

    .

---
+++ Alura_estudo/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,9 @@
+function tocaSom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSom;
+
+
---
+++ Alura_estudo/train.js/main.js
@@ -0,0 +1,3 @@
+document.querySelector('input[type=tel]');
+
+/* Exercício apenas para vermos como selecionar o elemento 'input' do tipo tel, colocamos o input entre aspas e parenteses, e o tipo entre colchetes ('input[type=tel]') */
